16637a4cb0cebdc72c9b9a02f4984274655bb69b
1RafaelSantana <rafael.que97@gmail.com>
2020-12-22 21:32:55 -0300

    config axios e vuetify

---
+++ client/src/components/AuthScreen.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="auth">
+		<div class="form-side">
+			<v-row>
+				<h2>Login Screen</h2>
+			</v-row>
+			<v-row class="subtitle">
+				<p>Seja bem vindo à tela de login! Informe o usuário e senha ou se cadastre para logar!</p>	
+			</v-row>			
+		</div>
+		<div class="image-side">
+			<h2>Here's the image side</h2>
+		</div>
+  </div>
+</template>
+
+<script>
+export default {
+	name: "LoginScreen"
+};
+</script>
+
+<style>
+.auth {
+	display: flex;
+}
+
+.form-side {
+	background-color: #dde9e9;
+	padding: 60px 45px;
+}
+
+.subtitle {
+	padding-top: 25px;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import "./config/toasted";
+import "./config/axios";
+import vuetify from "./config/vuetify";
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
